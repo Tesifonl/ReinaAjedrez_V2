ac527edd0ed04b3594b646a48695b4eebe6b7bac
Tesifonl <tesifonl@gmail.com>
2022-11-20 17:42:22 +0100

    CLASE POSICION COMMIT 2

--- src/main/java/tesi/Posicion.java
+++ src/main/java/tesi/Posicion.java
@@ -4,4 +4,35 @@ public class Posicion {
 	
 	   private int fila;
 	   private char columna;
-}
+	
+	
+	   
+	   public int getfila() {
+			return fila;
+		}
+
+	   public void setfila(int fila) {
+			if (fila < 1) {
+				throw new IllegalArgumentException("El valor de la fila es menor que el mínimo permitido (1).");
+			} else if (fila > 8) {
+				throw new IllegalArgumentException("El valor de la fila es mayor que el máximo permitido (8).");
+			}
+			this.fila = fila;
+		}
+
+		public int getcolumna() {
+			return columna;
+		}
+
+		public void setcolumna(char columna) {
+			if (columna<65) {
+				throw new IllegalArgumentException("El valor de la y es menor que el mínimo permitido.");
+			} else if (columna>71) { 
+				throw new IllegalArgumentException("El valor de la y es menor que el mínimo permitido.");
+			}	
+			this.columna = columna;
+		}
+		}
+	   
+	   
+
